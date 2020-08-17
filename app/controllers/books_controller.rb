b4f0b05171f17adf1d0297479a4c6fd38b306e52
Abdel Laqdim <al@kidsandplay.be>
2020-08-17 17:47:41 +0200

    add users routes, views, navbar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,15 +4,30 @@ class BooksController < ApplicationController
   end
 
   def show
-
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def my_books
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :language, :subject, :pages)
+  end
+
 end
