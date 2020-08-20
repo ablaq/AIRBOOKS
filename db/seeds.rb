ca76f1a816ecd386fed6bef267d1e2503bb1bc6f
Abdel Laqdim <al@kidsandplay.be>
2020-08-20 13:17:57 +0200

    search-bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,11 @@ user = User.create!(user_name: "ablaq", email: "al@airbooks.com", password: "123
 Book.create!(user: user, title: "la semaine de 4 heures", author: "Tim Ferris", language: "Fr", subject: "personal development", pages: 250)
 Book.create!(user: user, title: "Tools of Titans", author: "Tim Ferris", language: "En", subject: "personal development", pages: 530)
 Book.create!(user: user, title: "Four hours body", author: "Tim Ferris", language: "En", subject: "personal development", pages: 450)
+require 'json'
+require 'open-uri'
+
+url = 'https://api.github.com/users/ssaunier'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+puts "#{user['name']} - #{user['bio']}"
